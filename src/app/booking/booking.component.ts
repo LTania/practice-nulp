f7c94cfa987aacd0dfbe3672ebe783239c66e42c
LTania <tania.luchynska@gmail.com>
2020-06-06 15:09:23 +0300

    add booking

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, TemplateRef} from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
   styleUrls: ['./booking.component.sass']
 })
 export class BookingComponent implements OnInit {
+  date: string;
+  maxDateD: Date;
+  maxDate: string;
+  modalRef: BsModalRef;
 
-  constructor() { }
+  constructor(private modalService: BsModalService) {
+    this.date = new Date().toISOString().slice(0, 16);
+    this.maxDateD = new Date();
+    this.maxDateD.setDate(this.maxDateD.getDate() + 7);
+    this.maxDate = this.maxDateD.toISOString().slice(0, 16);
+  }
 
   ngOnInit(): void {
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+  confirm(): void {
+    this.modalRef.hide();
   }
 
 }
